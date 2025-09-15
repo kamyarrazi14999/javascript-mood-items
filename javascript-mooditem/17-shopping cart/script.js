a3812d8143b05e9275022d698410e06bd0acec4d
kamyarrazi14999 <kami6535@gmail.com>
2025-09-16 03:22:43 +0330

    new element

--- javascript-mooditem/17-shopping cart/script.js
+++ javascript-mooditem/17-shopping cart/script.js
@@ -1,2 +1,14 @@
-const careBUTton = document.getAnimations('header-cart-btn');
-const productslist = document.getAnimations('product-list-container');
+
+// ELEMENTS
+const cartButton = document.getElementById("header-cart-btn");
+const productContainer = document.getElementById("product-list-container"); 
+const cartcontainer = document.querySelector(".cart-container");
+
+
+
+// event listener
+cartButton.addEventListener ("click", () => { 
+    productContainer.classList.remove("show-section");
+    cartcontainer.classList.add("show-section");
+ 
+});
