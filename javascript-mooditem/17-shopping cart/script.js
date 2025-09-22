7089f3c9830d196fcdb480b3259f8a5e12a30039
kamyarrazi14999 <kami6535@gmail.com>
2025-09-23 00:01:16 +0330

    refactor: standardize variable naming and improve event listener structure

--- javascript-mooditem/17-shopping cart/script.js
+++ javascript-mooditem/17-shopping cart/script.js
@@ -1,19 +1,77 @@
-
-// ELEMENTS
+// Elements
 const cartButton = document.getElementById("header-cart-btn");
-const productContainer = document.getElementById("product-list-container"); 
-const cartcontainer = document.querySelector(".cart-container");
-const  headerlink= document.querySelector(".header-link");
+const productContainer = document.getElementById("product-list-container");
+const cartContainer = document.querySelector(".cart-container");
+const headerLink = document.querySelector(".header-link");
+const conditionText = document.querySelector(".condition-text");
+const productList = document.getElementById("product-list");
 
-
-
-// event listener
-cartButton.addEventListener ("click", () => { 
-    productContainer.classList.remove("show-section");
-    cartcontainer.classList.add("show-section");
- 
+// Show basket cart
+cartButton.addEventListener("click", () => {
+  productContainer.classList.remove("show-section");
+  cartContainer.classList.add("show-section");
 });
-headerlink.addEventListener ("click", () => {
-    cartcontainer.classList.remove("show-section");
-    productContainer.classList.add("show-section");
+
+// Show products list
+headerLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  cartContainer.classList.remove("show-section");
+  productContainer.classList.add("show-section");
 });
+
+// Fetch & get products from API
+const getProducts = async () => {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+
+    conditionText.style.display = "none"; // Hide Loading When Data Arrived
+   
+    renderPorducts(data); // Show Products
+  } catch (error) {
+    conditionText.textContent = error.message;
+    
+  }
+};
+
+// Function to render product in DOM
+const renderPorducts = (products) => {
+  products.map((product) => {
+    // create product item
+    const productElement = document.createElement("div");
+    productElement.className = "product";
+
+    // destructure product info
+    
+
+    productElement.innerHTML = `
+    <div class="product-img-wrapper">
+      <img
+        src="${image}"
+        alt="${shortenTitle(title)}"
+        class="product-img"
+      />
+      <div class="product-description">
+        <p>
+         ${description}
+        </p>
+      </div>
+    </div>
+    <h3 class="product-name">${shortenTitle(title)}</h3>
+    <div class="product-info-box">
+      <p class="product-price">$${price}</p>
+      <div>
+        <button class="add-to-cart-btn">Add to Cart</button>
+      </div>
+    </div>
+    `;
+
+    productList.appendChild(productElement);
+  });
+};
+
+// short products title
+
+
+// Start Initialy Project
+getProducts();
