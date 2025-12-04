02050f22161ba7684509a2f6097c3fc53b54b5b3
kamyarrazi14999 <kami6535@gmail.com>
2025-12-04 11:57:13 +0330

    new item innerHTML

--- javascript-mooditem/image api/main.js
+++ javascript-mooditem/image api/main.js
@@ -2,7 +2,6 @@
 const loadingBox = document.querySelector(".loading-img");
 const photosListContainer = document.getElementById("image-list-container");
 
-
 // aPI DATA
 const API_KEY = "guyzoV7LTTyH63GnYE15V57E07YLVArXfqiOWgN7ZOw";
 const API_URl = `https://api.unsplash.com/photos?page=1&client_id=${API_KEY}`;
@@ -12,8 +11,8 @@ const getImages = async (url) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-      showImages(data);
-      loadingBox.style.display = "none";
+    showImages(data);
+    loadingBox.style.display = "none";
   } catch (error) {
     photosListContainer.innerHTML = `h2 class='error-message'>${error.message}</h2>`;
     loadingBox.style.display = "none";
@@ -21,9 +20,19 @@ const getImages = async (url) => {
 };
 const showImages = (photos) => {
   photosListContainer.innerHTML = "";
+
   photos.forEach((photo) => {
-    const img = `
-   `
+    const { urls } = photo;
+    const image = `
+      <div class="image-item">
+        <img src="${urls.regular}" alt="Image"
+        class="image" 
+        />
+      
+
+  </div>
+    `;
+    photosListContainer.innerHTML += image;
   });
 };
 
