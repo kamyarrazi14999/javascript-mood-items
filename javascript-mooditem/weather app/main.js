44e68af5ecd37daca9052f11647710d7ec2dde43
kamyarrazi14999 <kami6535@gmail.com>
2025-12-14 01:10:35 +0330

    fix API URL protocol and refactor weather fetching function

--- javascript-mooditem/weather app/main.js
+++ javascript-mooditem/weather app/main.js
@@ -6,8 +6,8 @@ const weatherInfo = document.querySelector('.weather-form');
 
 
 // API DATA
-const API_KEY = 'f235a7d1451b2be4b94e8ad0ce5fb084';
-const API_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&q=`;
+const API_KEY = "f235a7d1451b2be4b94e8ad0ce5fb084";
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&q=`;
 weatherInfo.addEventListener('submit', (e) => {
   e.preventDefault();
     if (cityInput.value) {
@@ -15,11 +15,9 @@ weatherInfo.addEventListener('submit', (e) => {
   }
 });
 const getWeater = async (url) => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}  
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
