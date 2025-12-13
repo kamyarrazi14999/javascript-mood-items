109fc79dcf23b4532009dce267c0a272dc4d3e48
kamyarrazi14999 <kami6535@gmail.com>
2025-12-13 13:45:45 +0330

    add weather fetching functionality

--- javascript-mooditem/weather app/main.js
+++ javascript-mooditem/weather app/main.js
@@ -1,1 +1,25 @@
+// ELEMENTS SELECTION
+const cityInput = document.getElementById('city-input');
+const weatherInfo = document.querySelector('.weather-form');
 
+
+
+
+// API DATA
+const API_KEY = 'f235a7d1451b2be4b94e8ad0ce5fb084';
+const API_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&q=`;
+weatherInfo.addEventListener('submit', (e) => {
+  e.preventDefault();
+    if (cityInput.value) {
+     getWeater(API_URL + cityInput.value)
+  }
+});
+const getWeater = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}  
