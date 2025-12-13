2f508ed7912cbdf7ab3c8ca09222d27a11498713
kamyarrazi14999 <kami6535@gmail.com>
2025-12-14 01:18:18 +0330

    fix error message handling and improve code formatting

--- javascript-mooditem/weather app/main.js
+++ javascript-mooditem/weather app/main.js
@@ -1,23 +1,30 @@
 // ELEMENTS SELECTION
-const cityInput = document.getElementById('city-input');
-const weatherInfo = document.querySelector('.weather-form');
-
-
-
+const cityInput = document.getElementById("city-input");
+const weatherInfo = document.querySelector(".weather-form");
+eroreMassage = document.querySelector(".error-message");
 
 // API DATA
 const API_KEY = "f235a7d1451b2be4b94e8ad0ce5fb084";
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&q=`;
-weatherInfo.addEventListener('submit', (e) => {
+weatherInfo.addEventListener("submit", (e) => {
   e.preventDefault();
-    if (cityInput.value) {
-     getWeater(API_URL + cityInput.value)
+  if (cityInput.value) {
+    getWeater(API_URL + cityInput.value);
   }
 });
 const getWeater = async (url) => {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      if (data.cod === "404") {
+        eroreMassage.style.display = "block"; // show error message
+        eroreMassage.textContent = data.message;
+      }
+      else { 
+        eroreMassage.style.display = "none"; // hide error message
+        eroreMassage.textContent = "";
+        cityInput.value = "";
+      }
+
     });
 };
